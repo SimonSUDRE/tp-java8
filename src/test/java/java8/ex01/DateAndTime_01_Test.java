92f4268ea7643b44b72b788191b2077f31fd378c
SimonSUDRE <sta.sudre@gmail.com>
2018-01-09 16:37:44 +0100

    Date & Time API (branche : 06-date-and-time) fin

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -14,17 +14,19 @@ import static org.junit.Assert.assertThat;
 public class DateAndTime_01_Test {
 
     // Vous vous souvenez de java.util.Date (Java 1) ?
-    @Test
+    @SuppressWarnings("deprecation")
+	@Test
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(117, 2, 24, 1, 2, 3);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
-
+        date = new Date(117, 2, 25, 1, 2, 3);
+        
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
@@ -35,11 +37,13 @@ public class DateAndTime_01_Test {
         Calendar calendar = Calendar.getInstance();
 
         // TODO modifier l'objet calendar pour que le test soit passant
+        calendar.set(2017, 2, 24, 1, 2, 3);
 
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
+        calendar.set(2017, 2, 25, 1, 2, 3);
 
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -1,12 +1,11 @@
 package java8.ex07;
 
 import org.junit.Test;
-
-import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -15,17 +14,18 @@ import static org.junit.Assert.assertThat;
  */
 public class DateAndTime_07_Test {
 
-    @Test
+    @SuppressWarnings("deprecation")
+	@Test
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date(117, 1, 12);
 
         // TODO transformer la date en Instant
-
+        Instant instant = date.toInstant();
+        
         // TODO transformer la date en LocalDate
-
-        LocalDate result = null;
+        LocalDate result = instant.atZone(ZoneId.systemDefault()).toLocalDate();
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
